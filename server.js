93dead667482946fb7fd0ea27faf3a6883bc1e90
Maham Waseem <mahamw440@gmail.com>
2025-06-30 15:23:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,7 @@ const PORT = 4000;
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to the Home Page!</h1>');
 });
+app.get('/about', (req, res) => {
+    res.send('<h1>About Us</h1><p>This is the About page.</p>');
+});
+
