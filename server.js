c8a9277c331a3681492f9fe8a9806a985d6929b1
Maham Waseem <mahamw440@gmail.com>
2025-06-30 15:25:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,3 +8,9 @@ app.get('/about', (req, res) => {
     res.send('<h1>About Us</h1><p>This is the About page.</p>');
 });
 
+app.get('/contact', (req, res) => {
+    res.send('<h1>Contact Us</h1><p>Feel free to reach out!</p>');
+});
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
