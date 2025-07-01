f5e46f2f8298b9bdaa59e21c4aa76ec25117ce76
Maham Waseem <mahamw440@gmail.com>
2025-07-01 12:48:35 +0500

    Update Asynchronous.js

--- Asynchronous.js
+++ Asynchronous.js
@@ -3,10 +3,13 @@ const app = express();
 
 app.get('/', async(req , res) => {
   try{
-    const data = await fetchdatafromdatabase();
+    const data = await fetchDataFromDatabase();
     res.json(data);
   }
   catch(err){
     res.status(500).semd('Error fetching data');
   }
 });
+async function  fetchDataFromDatabase(){
+  return { id : 1, name : 'Product A'};
+}
