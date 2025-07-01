ad752e7c6a938a805b69fd3e780ed9aa376a1941
Maham Waseem <mahamw440@gmail.com>
2025-07-01 15:07:54 +0500

    Update app_mountpath.js

--- app_mountpath.js
+++ app_mountpath.js
@@ -1,1 +1,18 @@
 //The app.mountpath property contains one or more path patterns on which a sub-app was mounted. 
+
+const express = require('express');
+const app = express();  // the main app
+const user = express(); // the sub app
+const PORT = 3000;
+
+user.get('/', function (req, res) {
+      console.log(user.mountpath); // /user
+      res.send('User Homepage');
+});
+
+app.use('/user', user); // Mounting the sub app
+
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
