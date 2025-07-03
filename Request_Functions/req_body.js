658f7403bc67d99d1bb962735240e7c9e2767be9
Maham Waseem <mahamw440@gmail.com>
2025-07-03 11:45:33 +0500

    Create req_body.js

---
+++ Request_Functions/req_body.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/signup', function (req, res) {
+    const data = req.body;
+
+    console.log("Name: ", data.name);
+    console.log("Age: ", data.age);
+    console.log("Gender: ", data.gender);
+
+    res.send();
+});
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
