b692f87d98809f51cc277f81dd32cd216d7b316f
Maham Waseem <mahamw440@gmail.com>
2025-07-03 11:33:51 +0500

    Update req_baseurl_property.js

--- Request_Functions/req_baseurl_property.js
+++ Request_Functions/req_baseurl_property.js
@@ -1,1 +1,23 @@
 //The req.baseUrl property is the URL path on which a router instance was mounted. The req.baseUrl property is similar to the mount path property of the app object, except app.mountpath returns the matched path pattern(s). 
+const express = require('express');
+const e = require('express');
+const app = express();
+const PORT = 3000;
+
+const student = express.Router();
+app.use('/student', student);
+
+student.get('/signup', function (req, res) {
+    if (req.baseUrl == '/student') {
+        console.log("Show Signup Form");
+        res.end();
+    } else {
+        console.log("Invalid Request")
+        res.send("Invalid Route")
+    }
+})
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
