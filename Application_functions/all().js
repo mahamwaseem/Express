5f12a59d55516098f14fb96207c3d6480761f6b8
Maham Waseem <mahamw440@gmail.com>
2025-07-03 11:02:50 +0500

    Update all().js

--- Application_functions/all().js
+++ Application_functions/all().js
@@ -6,3 +6,7 @@ app.all('/user', function(req, ews, next) {
   console.log('USER API CALLED');
   next();
 });
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT ", PORT);
+});
